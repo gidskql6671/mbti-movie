4e17bf9ca6c4bc7867145fb2aa92c20a562d57dc
gidskql6671 <gidskql6671@gmail.com>
2022-12-03 04:53:57 +0900

    GET /api/description 구현

---
+++ backend/src/main/java/com/example/mbtimovie/dto/GetDescriptionResponse.java
@@ -0,0 +1,10 @@
+package com.example.mbtimovie.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GetDescriptionResponse {
+    private final String description;
+}
