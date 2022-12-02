9339b235f98b2acaffe44e490e7ceea158bce253
gidskql6671 <gidskql6671@gmail.com>
2022-12-03 01:23:03 +0900

    backend init

---
+++ backend/src/main/java/com/example/mbtimovie/MbtiMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbtimovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtiMovieApplication.class, args);
+    }
+
+}
