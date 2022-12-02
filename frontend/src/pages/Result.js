b211f2065a0421a30cdb9cebb9db862c8466516b
gidskql6671 <gidskql6671@gmail.com>
2022-12-03 07:14:10 +0900

    영화 리스트 밑 장르를 가져오도록 구현

--- frontend/src/pages/Result.js
+++ frontend/src/pages/Result.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useLocation } from "react-router-dom";
 import '../styles/Result.css';
 
 const Result = () => {
-    // fetch("https://mbti-movie.herokuapp.com/api/movie_list?mbti=entp").then((res) => res.json()).then(
-    //     res => console.log(res)
-    // )
+    const location = useLocation();
+    const mbti = location.state.mbti
+
+    const [genre, setGenre] = useState("뭘까?");
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const domain = "https://mbti-movie.herokuapp.com"
+
+        fetch(`${domain}/api/movie_list?mbti=${mbti}`).then((res) => res.json()).then(
+            res => setMovies(res.movies)
+        )
+
+        fetch(`${domain}/api/genre?mbti=${mbti}`).then((res) => res.json()).then(
+            res => setGenre(res.genre)
+        )
+      },[mbti]);
+
     return (
         <div className="result-container">
-            <div>ENTP 가 좋아할 영화</div>
+            <div> 너가 좋아하는 장르는? {genre} </div>
+            <div> {mbti} 가 좋아할 영화 </div>
+            <ul>
+                {
+                    movies.map((movie, index) =>
+                        <li class="movie" key={index}> {movie.title} </li>
+                    )
+                }
+            </ul>
         </div>
     );
 }
